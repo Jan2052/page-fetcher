ad90e73e9d00ab5aeb05bd93c4beb83cb8b250f0
jan2052 <janet_lam@live.ca>
2022-08-22 23:46:32 -0400

    console.logged file size and threw error line20-21

--- fetcher.js
+++ fetcher.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
 const args = process.argv.slice(2)
-console.log(args)
+// console.log(args)
 
-let url = args[0]
-let localFilePath = args[1]
+let url = args[0];
+let localFilePath = args[1];
 
 const request = require('request');
 request(url, (error, response, body) => {
   if (error) console.log('Error on HTTP GET Request...', error);
   //  console.log('response', response);
   const { statusCode } = response;
-  console.log('status code', statusCode)
+  console.log('status code', statusCode);
   console.log('body', body);
   // if (statusCode === 200) {
   //   const json = JSON.parse(body)
   //   console.log(json)
   // }
   fs.writeFile(localFilePath, body, err => {
-    if (err) {
-      console.error(err);
-    }
+    if (err) throw err;
+    console.log(`Downloaded and saved ${body.length} bytes to ./index.html`);
   });
-}); s
+});
+
