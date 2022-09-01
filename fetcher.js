30433951445bbc5323319f6cf363b7d7479a13cf
jan2052 <janet_lam@live.ca>
2022-09-01 19:13:26 -0400

    Completed Edge Cases

--- fetcher.js
+++ fetcher.js
@@ -1,25 +1,46 @@
 const fs = require('fs');
-
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 let url = process.argv[2];
 let localFilePath = process.argv[3];
 
 const request = require('request');
 request(url, (error, response, body) => {
-  if (error) console.log('Error on HTTP GET Request...', error);
-  //  console.log('response', response);
-  const { statusCode } = response;
-  if (statusCode && statusCode !== 200){
-    console.log(`Status code: ${statusCode}`)
+  if (error) {
+    console.log('Error on HTTP GET Request...', error);
     return;
   }
-  // console.log('status code', statusCode);
-  // console.log('body', body);
-  // if (statusCode === 200) {
-  //   const json = JSON.parse(body)
-  //   console.log(json)
-  // }
-  fs.writeFile(localFilePath, body, err => {
-    if (err) throw err;
-    console.log(`Downloaded and saved ${body.length} bytes to ./index.html`);
+
+  if (response && response.statusCode !== 200) {
+    console.log(`Status code: ${response.statusCode}`);
+    return;
+  }
+
+  const writefile = function() {
+    fs.writeFile(localFilePath, body, err => {
+      if (err) throw err;
+      console.log(`Downloaded and saved ${body.length} bytes to ./index.html`);
+    });
+  };
+
+  if (!fs.existsSync(localFilePath)) {
+    writefile();
+    rl.close();
+  }
+
+  rl.question('File already exists. Replace existing file? (y/n)', (answer) => {
+    if (answer === "n") {
+      console.log("File was not downloaded");
+      rl.close();
+    } else if (answer === "y") {
+      writefile();
+      rl.close();
+    } else {
+      console.log('Invalid Input');
+      rl.close();
+    }
   });
 });
